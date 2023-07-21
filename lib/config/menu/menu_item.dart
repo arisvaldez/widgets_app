ace82ccd28804b8ae9a270c5ddfa499468bd9521
Aris Valdez <samus45@hotmail.es>
2023-07-20 22:18:20 -0400

    add theme colors

---
+++ lib/config/menu/menu_item.dart
@@ -0,0 +1,13 @@
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final String icon;
+
+  MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+enum ThemeColors { blue, green, red, pink, orange, teal, purple, yellow }
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.green,
+  Colors.red,
+  Colors.pink,
+  Colors.orange,
+  Colors.teal,
+  Colors.purple,
+  Colors.yellow,
+];
+
+class AppTheme {
+  final ThemeColors selectedColor;
+
+  AppTheme({required this.selectedColor});
+
+  ThemeData getTheme() => ThemeData(
+      useMaterial3: true, colorSchemeSeed: colorList[selectedColor.index]);
+}
