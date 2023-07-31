035529e3a20c43dfb24f397efcd06b45f471d0fd
Aris Valdez <samus45@hotmail.es>
2023-07-31 18:31:13 -0400

    add border to buttoms

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -39,6 +39,8 @@ class _CardsView extends StatelessWidget {
               _CardType2(elevation: card['elevation'], label: card['label'])),
           ...cards.map((card) =>
               _CardType3(elevation: card['elevation'], label: card['label'])),
+          ...cards.map((card) =>
+              _CardType4(elevation: card['elevation'], label: card['label'])),
           const SizedBox(height: 50),
         ],
       ),
@@ -149,6 +151,44 @@ class _CardType3 extends StatelessWidget {
           )
         ]),
       ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType4({
+    required this.elevation,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(children: [
+        Image.network(
+          'https://picsum.photos/id/${elevation.toInt()}/600/350',
+          height: 350,
+          fit: BoxFit.cover,
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Container(
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(20))),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_time_rounded),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
