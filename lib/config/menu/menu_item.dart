0313ba68429941e698e34334d3c354108d4fefe0
Aris Valdez <samus45@hotmail.es>
2023-08-02 08:43:33 -0400

    add Animated Container

--- lib/config/menu/menu_item.dart
+++ lib/config/menu/menu_item.dart
@@ -30,9 +30,14 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Generals and controlled',
       link: '/progress',
       icon: Icons.refresh_rounded),
-       MenuItem(
+  MenuItem(
       title: 'Snackbars',
       subTitle: 'Snackbar and dialogs',
       link: '/snackbars',
-      icon: Icons.refresh_rounded)
+      icon: Icons.refresh_rounded),
+  MenuItem(
+      title: 'Animated Container',
+      subTitle: 'Animated Container',
+      link: '/animated',
+      icon: Icons.check_box_outline_blank_rounded),
 ];
